0132e418cb0e6949f791ed51928000553f91ee44
quekcg <quekcg3107@gmail.com>
2021-02-16 11:19:10 +0800

    updating package json with nodemon dev server

--- server.js
+++ server.js
@@ -24,13 +24,18 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send('it is working') })
+app.get('/', (req, res) => { res.send(database.users) })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
-app.listen(process.env.PORT || 3001, () => {
-	console.log(`app is running on port ${process.env.PORT}`);
+app.listen( 3001, () => {
+	console.log(`app is running on port 3001`);
 })
+
+// app.get('/', (req, res) => { res.send('it is working') })
+
+// app.listen(process.env.PORT || 3001, () => {
+	// console.log(`app is running on port ${process.env.PORT}`);
